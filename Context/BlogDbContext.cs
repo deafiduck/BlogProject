7c5810816d6c70a6e08bac8e9c0919a7c5a739d7
deafiduck <110257389+deafiduck@users.noreply.github.com>
2025-08-23 00:07:03 +0300

    template changes, connetc db

---
+++ Context/BlogDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BlogV1.Models;
+namespace BlogV1.Context
+{
+    public class BlogDbContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-N5A035R; database=BlogV1; integrated Security=True;TrustServerCertificate=True;");
+        }
+        //veri tabanı bağlantısı
+
+        public DbSet<Blog> Blogs { get; set; }
+    }
+}
